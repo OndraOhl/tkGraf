e734691b054a1dc8e5c14e4419f6cabfae41d5a3
OndraOhl <ohl39077@spseol.cz>
2023-03-06 13:54:29 +0100

    rrj

--- interpolace.py
+++ interpolace.py
@@ -11,5 +11,18 @@ y= "0 0.1 0.5 1   3 10 30".split()
 x = list(map(float, x))
 y = list(map(float, y))
 
-lab.plot(x, y, 'x')
+lab.plot(x, y, "o", color="red")
+
+spl = inp.CubicSpline(x, y)
+newX = lab.linspace(0, 3, 2000)
+newY = spl(newX)
+lab.plot(newX, newY, ":", label="CubicSpline")
+
+spl = inp.UnivariateSpline(x,y, k=4)
+newY = spl(newX)
+lab.plot(newX, newY, ":", label="make_interspline")
+
+
+lab.grid()
+lab.legend()
 lab.show()
