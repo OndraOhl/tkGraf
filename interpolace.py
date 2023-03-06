6c37119fe409f0d648c949fc91a09babb8978fa5
OndraOhl <ohl39077@spseol.cz>
2023-03-06 13:31:12 +0100

    aehgrštčetčer

--- interpolace.py
+++ interpolace.py
@@ -7,3 +7,9 @@ y= [0, 0.1, 0.5, 1,   3, 10, 30]
 #nebo
 x= "0 0.3 0.5 0.8 1  2  3".split()
 y= "0 0.1 0.5 1   3 10 30".split()
+
+x = list(map(float, x))
+y = list(map(float, y))
+
+lab.plot(x, y, 'x')
+lab.show()
